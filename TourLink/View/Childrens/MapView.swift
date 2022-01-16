12c2cc77f1a245a39292d87a95845a9d222eb1e4
Luong Thuan Chung <53853568+LuongThuanChung@users.noreply.github.com>
2022-01-16 15:34:37 +0700

    save

--- TourLink/View/Childrens/MapView.swift
+++ TourLink/View/Childrens/MapView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Luong Thuan Chung on 14/01/2022.
 //
-
+import CoreGraphics
 import SwiftUI
 import MapKit
 
@@ -67,6 +67,38 @@ struct MapView: UIViewRepresentable {
             return nil
                 
             
+        }
+        
+        
+        
+        
+        //=======TEST====//
+        func mapView(_ mapView: MKMapView, didAdd views: [MKAnnotationView]) {
+            if views.last?.annotation is MKUserLocation {
+                addHeadingView(toAnnotationView: views.last!)
+            }
+        }
+        
+       
+        func addHeadingView(toAnnotationView annotationView: MKAnnotationView) {
+            var headingImageView: UIImageView?
+           
+            if let image = UIImage.init(named: "BlueArrowIcon")
+            {
+                
+                headingImageView = UIImageView(image: image)
+                
+                headingImageView!.frame = CGRect(x: (annotationView.frame.size.width - image.size.width)/2,
+                                                 y: (annotationView.frame.size.height - image.size.height)/2,
+                                                 width: image.size.width,
+                                                 height: image.size.height )
+                //headingImageView?.backgroundColor = .red
+                
+                annotationView.insertSubview(headingImageView!, at: 0)
+                //headingImageView!.isHidden = true
+            }
+                
+             
         }
         
         
